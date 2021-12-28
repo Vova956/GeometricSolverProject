b02537d8d6f9825b22e9e7ae6e7fe9a6169aa0e7
Vova956 <vladimrvarga2019#gmail.com>
2021-12-28 16:39:18 +0200

    Fixed bugs

--- app/src/main/java/com/example/geometricsolver3/MainActivity.java
+++ app/src/main/java/com/example/geometricsolver3/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
